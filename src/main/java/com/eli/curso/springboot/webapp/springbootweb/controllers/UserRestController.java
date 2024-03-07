3e0362f7aa797092645b9cea5f96548b245560a8
Elizabeth <e.mora@anoki.it>
2024-03-07 10:13:38 +0100

    created class 'UserDto'

--- src/main/java/com/eli/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
+++ src/main/java/com/eli/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
@@ -8,18 +8,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eli.curso.springboot.webapp.springbootweb.models.User;
+import com.eli.curso.springboot.webapp.springbootweb.models.dto.UserDto;
 
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
 
     @GetMapping("/details")
-    public Map<String, Object> details() {
+    public UserDto details() {
+        UserDto userDto = new UserDto();
+        User user = new User("Eli", "Mora");
+        userDto.setUser(user);
+        userDto.setTitle("Hola mundo");
+        return userDto;
+    }
+
+    @GetMapping("/details-map")
+    public Map<String, Object> detailsMap() {
         User user = new User("Eli", "Mora");
         Map<String, Object> body = new HashMap<>();
         body.put("title", "Hola mundo");
         body.put("user", user);
         return body;
     }
+
 
 }
