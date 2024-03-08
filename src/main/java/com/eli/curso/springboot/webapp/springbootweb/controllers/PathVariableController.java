b8a8b2bc741f59c8c1cfdd92beed4b071ed3b0c8
Eli <147066946+moraelizabeth@users.noreply.github.com>
2024-03-08 11:11:42 +0100

    Update PathVariableController

--- src/main/java/com/eli/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
+++ src/main/java/com/eli/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
@@ -1,5 +1,24 @@
 package com.eli.curso.springboot.webapp.springbootweb.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eli.curso.springboot.webapp.springbootweb.models.dto.ParamDto;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@RestController
+@RequestMapping("/api/var")
 public class PathVariableController {
 
+    @GetMapping("baz/{message}")
+    public ParamDto baz(@PathVariable String message) {
+        
+        ParamDto param = new ParamDto();
+        param.setMessage(message);
+        return param;
+    }
+    
 }
