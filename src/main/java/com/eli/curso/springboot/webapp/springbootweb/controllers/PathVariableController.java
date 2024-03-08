f3f95e8487658bd0200e0537034e4b115a4c9633
Elizabeth <e.mora@anoki.it>
2024-03-08 12:32:38 +0100

    value injection

--- src/main/java/com/eli/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
+++ src/main/java/com/eli/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
@@ -3,15 +3,32 @@ package com.eli.curso.springboot.webapp.springbootweb.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eli.curso.springboot.webapp.springbootweb.models.User;
 import com.eli.curso.springboot.webapp.springbootweb.models.dto.ParamDto;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
+
+    @Value("${config.code}")
+    private Integer code;
+    @Value("${config.username}")
+    private String username;
+    @Value("${config.listOfValues}")
+    private String[] listOfValues;
 
     @GetMapping("baz/{message}")
     public ParamDto baz(@PathVariable String message) {
@@ -29,4 +46,24 @@ public class PathVariableController {
         param.setCode(code);
         return param;
     }
+
+    @PostMapping("/create")
+    public User create(@RequestBody User user) {
+        //save in DB
+        return user;
+    }
+
+    @GetMapping("values")
+    public Map<String, Object> values(@Value("${config.message}") String message) {
+        //value injection
+        Map<String, Object> json = new HashMap<String, Object>();
+        
+        json.put("code", code);
+        json.put("username", username);
+        json.put("message", message);
+        json.put("listOfValues", listOfValues);
+        return json;
+    }
+    
+    
 }
